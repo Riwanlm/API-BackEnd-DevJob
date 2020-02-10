0d5ec4645bda85cd570cf2fb1e3e765ec523f810
pjehan <pierre.jehan@gmail.com>
2020-02-10 15:56:28 +0100

    Ajout d'un filtre sur le slug de l'entité Job

--- src/Entity/Job.php
+++ src/Entity/Job.php
@@ -2,16 +2,19 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Gedmo\Mapping\Annotation as Gedmo;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\JobRepository")
  * @ApiResource(normalizationContext={"groups": {"read"}})
+ * @ApiFilter(SearchFilter::class, properties={"slug": "exact"})
  * @ORM\HasLifecycleCallbacks
  */
 class Job
